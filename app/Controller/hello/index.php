d50aba2f20abdb9286e96d17e983edf54e40abff
ohs70230 <t.aroo1512@gmail.com>
2019-08-08 10:59:24 +0900

    firts commit

---
+++ app/Controller/hello/index.php
@@ -0,0 +1,10 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// TOPページのコントローラ
+$app->get('/hello', function (Request $request, Response $response) {
+  $data = [];
+  return $this->view->render($response, 'hello/index.twig', $data);
+});
