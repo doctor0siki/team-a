bacf53010b7d30d1d847c86c3f6bb4fd7a87f069
tsurusekazuki <b1714935@planet.kanazawa-it.ac.jp>
2019-08-08 17:58:07 +0900

    purchase and trade

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -2,13 +2,36 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\Payinfo;
+use Model\Dao\Users;
+use Model\Dao\Items;
+use Model\Dao\Univ;
 
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
 
-    $data = [];
+  $sql = "select
+Users.name as username,
+Items.name as itemname,
+Univ.*,
+Users.*,
+Items.*,
+Payinfo.*
+from Users
+inner join Payinfo
+on Users.id = Payinfo.id
+inner join Items
+on Items.id = Payinfo.id
+inner join Univ
+on Univ.id = Payinfo.id
+;";
+
+  $stmt = $this->db->prepare($sql);
+  $stmt->execute();
+  $data = $stmt->fetchAll();
+  $users = [];
+  $users['users'] = $data;
 
     // Render index view
-    return $this->view->render($response, 'top/index.twig', $data);
+    return $this->view->render($response, 'top/index.twig', $users);
 });
-
--- app/Controller/trade/index.php
+++ app/Controller/trade/index.php
@@ -1,17 +1,36 @@
 <?php
-
+use Doctrine\DBAL\Query\QueryBuilder;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\Payinfo;
 use Model\Dao\Users;
 use Model\Dao\Items;
+use Model\Dao\Univ;
 
 // TRADEページのコントローラ
 $app->get('/trade', function (Request $request, Response $response) {
 
-    $data = [];
-    
+    $sql = "select
+Users.name as username,
+Items.name as itemname,
+Univ.*,
+Users.*,
+Items.*,
+Payinfo.*
+from Users
+inner join Payinfo
+on Users.id = Payinfo.id
+inner join Items
+on Items.id = Payinfo.id
+inner join Univ
+on Univ.id = Payinfo.id
+;";
 
-    // Render index view
-    return $this->view->render($response, 'trade/trade.twig', $data);
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll();
+    $users = [];
+    $users['users'] = $data;
+
+    return $this->view->render($response, 'trade/trade.twig', $users);
 });
