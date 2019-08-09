e34d943293b2c5b3d77d8bef6abe313fdb6857b3
ohs70230 <t.aroo1512@gmail.com>
2019-08-09 10:48:57 +0900

    取引完了画面処理のコメントを記述

--- app/Controller/transaction/index.php
+++ app/Controller/transaction/index.php
@@ -29,7 +29,7 @@ $app->get('/transaction/', function (Request $request, Response $response) {
 $app->get('/transaction/detail', function (Request $request, Response $response) {
   // getで取引ID、sessionでuser_idを取得する
   $get_data = $request->getQueryParams(); // $get_data['id']  商品ID
-  $session = $this->session["user_info"]; // $session['id'] 買い手側のユーザーID
+//  $session = $this->session["user_info"]; // $session['id'] 買い手側のユーザーID
 
   // 取得した取引IDでPayinfoテーブルを検索
   // 必要なのは、商品ID・商品名・商品説明・商品価格・payout_date
@@ -43,26 +43,21 @@ $app->get('/transaction/detail', function (Request $request, Response $response)
   $stmt->execute();
   $result = $stmt->fetchAll();
 
-
-  return $this->view->render($response, 'transaction/detail.twig', $data);
+  // $result内のデータ郡を表示する。
+  return $this->view->render($response, 'transaction/detail.twig', $result);
 });
 
 // 取引完了画面コントローラ
 $app->post('/transaction/complete', function (Request $request, Response $response) {
+  // 取引完了ボタンが押された時の処理
 
-  // 商品IDをgetリクエストから取得
+  // 取引IDをpostリクエストから取得
   $get_data = $request->getQueryParams();
 
-  // 取得したユーザーIDでPayinfoテーブルを検索
-  $sql = "
-    SELECT I.id, I.name, I.describe, I.price, P.transaction_status, U.id AS user_id, U.name AS user_name
-    FROM Payinfo P
-        INNER JOIN Users U ON P.user2_id = U.id
-        INNER JOIN Items I on P.item_id = I.id
-    WHERE P.item_id = ".$get_data['id'];
-  $stmt = $this->db->prepare($sql);
-  $stmt->execute();
-  $result = $stmt->fetchAll();
+  // 商品価格分の料金データを売り手userのmoneyに追加する。
+  // 売り手userのmoneyを取得
+
+  // Payinfoのpayout_dateとtrasaction_statusを更新する
 
   // セッションのデータ(user_id)でItemsテーブルを参照し、取引しているすべての商品の情報を取得する。
   return $this->view->render($response, 'transaction/detail.twig', $result);
