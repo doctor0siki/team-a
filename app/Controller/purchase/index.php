739e50b9fc16ae69555b922897b8d24a6da66a2e
町田 直基 <n-machida@ceres-inc.jp>
2019-08-09 15:49:07 +0900

    bug fix

--- app/Controller/purchase/index.php
+++ app/Controller/purchase/index.php
@@ -11,36 +11,21 @@ use Model\Dao\Univ;
 // TRADEページのコントローラ
 $app->get('/purchase/{id}/', function (Request $request, Response $response, $args) {
 
-  $sql = "select
-Users.name as username,
-Items.name as itemname,
-Univ.*,
-Users.*,
-Items.*,
-Payinfo.*
-from Users
-inner join Payinfo
-on Users.id = Payinfo.id
-inner join Items
-on Items.id = Payinfo.id
-inner join Univ
-on Univ.id = Payinfo.id
-where Users.id = ?
-;";
-  $id = $args['id'];
+    $itemId = $args['id'];
 
-  $stmt = $this->db->prepare($sql);
-  $stmt->bindValue(1, $id);
-  $stmt->execute();
+    $sql = "select
+    Items.user_id,
+    from Items 
+    where Items.id = ?
+    ;";
 
-  $data = $stmt->fetch();
-  $users = [];
-  $users['user'] = $data;
+    $stmt = $this->db->prepare($sql);
+    $stmt->bindValue(1, $itemId);
+    $stmt->execute();
+    $data = $stmt->fetch();
 
-
-
-  // ログインIDをセッションから取得
-  $session['login_id'] = 2;     // 2は例
+    $user1_id = $data['user_id'];
+    $user2_id = $this->session["user_info"]['id'];
 
   // Payinfoに購入情報を登録
   // 必要な情報…… user1_id:session_id, user2_id:I.user_id, item_id:I.id
@@ -50,23 +35,23 @@ where Users.id = ?
     VALUES 
         (:user1_id, :user2_id, :item_id, 0, NOW())";
   $stmt = $this->db->prepare($sql);
-  $params = array(':user_id' => $data['user_id'],
-                  ':user2_id' => $session['login_id'],
-                  'item_id' => $get_data['id']);
+  $params = array(':user_id' => $user1_id,
+                  ':user2_id' => $user2_id,
+                  'item_id' => $itemId);
   $stmt->execute($params);
 
   // Itemsのステータスも変更しておく。1:取引完了(sold状態)
   $sql = "
         UPADTE Items
          SET payout_state = 1
-         WHERE id = ".$get_data['id'];
+         WHERE id = ".$itemId;
   $stmt = $this->db->prepare($sql);
   $stmt->execute();
 
 
   // 購入者側のmoneyを引いておく。
   // 購入者のmoneyを取得
-  $sql = "SELECT money FROM Users WHERE id = ".$session['login_id'];
+  $sql = "SELECT money FROM Users WHERE id = ".$user2_id;
   $stmt = $this->db->prepare($sql);
   $stmt->execute();
   $data_cal = $stmt->fetchAll();
@@ -76,11 +61,11 @@ where Users.id = ?
 
   // 残った額でUPDATE
   $sql = "UPDATE Users 
-            SET money = {$data_cal['money']}
-            WHERE id = {$session['login_id']}";
+            SET money = " . $data_cal['money'] .
+            " WHERE id = " .$user2_id;
   $stmt = $this->db->prepare($sql);
   $stmt->execute();
 
     // Render index view
-    return $this->view->render($response, 'purchase/purchase.twig', $users);
+    return $this->view->render($response, 'thanks/thanks.twig');
 });
